39c66ba4e582e0e3e52f31ebc10d397b9182e4a2
witampanstwa <robert.dziubek5@gmail.com>
2021-03-15 15:28:11 +0100

    Update

--- resources/js/component/task.js
+++ resources/js/component/task.js
@@ -4,12 +4,12 @@ class Task {
      * @param {Date} timeAllocated Specified in whole hours.
      */
     constructor(name, timeAllocated) {
-        //let now = Date.now();
+        let now = Date.now();
 
         this._name = name;
         this._timeAllocated = timeAllocated;
         this._addedAt = now;
-        this._pastDue = 0; //now.setHours(now.getHours() + timeAllocated);
+        this._pastDue = now.setHours(now.getHours() + timeAllocated);
     }
 
     get name() {
